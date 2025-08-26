c70bb154cafb7ba1ff5398dbc59c87c816d32f43
Matthias Wilhelm <matt@mwil.me>
2025-08-26 22:02:54 +0900

    Fixed fugitive binding to what I used before.

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('n', '<Leader>gg', '<Cmd>Git<CR>', { desc = 'Git Status' })
-vim.keymap.set('n', '<Leader>gu', '<Cmd>Git push<CR>', { desc = 'Git Push' })
-vim.keymap.set('n', '<Leader>gp', '<Cmd>Git pull<CR>', { desc = 'Git Pull' })
+vim.keymap.set('n', '<Leader>gp', '<Cmd>Git push<CR>', { desc = 'Git Pull' })
+vim.keymap.set('n', '<Leader>gu', '<Cmd>Git pull<CR>', { desc = 'Git Push' })
 vim.keymap.set('n', '<Leader>go', '<Cmd>Git checkout<CR>', { desc = 'Git Checkout' })
 
 return {
